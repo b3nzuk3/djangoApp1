d6935766f2dd85fb836b775035f8fb1f0a7215e4
b3nzuk3 <hussenito7@gmail.com>
2022-05-31 04:12:25 +0300

    created model classes for the images

--- imageGram/models.py
+++ imageGram/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Image(models.Model):
+        name = models.CharField(max_length=200)
+        location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+        category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+        picture = CloudinaryField('picture')
+        description = models.TextField(null=True, blank=True)
+        created = models.DateTimeField(auto_now_add=True)
+        class Meta:
+            ordering = ['-created']
+        def __str__(self):
+            return self.name
+
+
+class Location(models.Model):
+        name = models.CharField(max_length=200)
+        def __str__(self):
+            return self.name
+
+class Category(models.Model):
+        name = models.CharField(max_length=200)
+        def __str__(self):
+            return self.name
