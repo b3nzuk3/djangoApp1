dd9af11697964b7057f5b9625ed2164802e17e5f
b3nzuk3 <hussenito7@gmail.com>
2022-05-31 04:21:53 +0300

    configured cloudinary for use in application

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config(
+  cloud_name = "dw1ntqyia",
+  api_key = "791785238298589",
+  api_secret = "WPQ2gU31-wyycuPi3QHe4gPcsRY"
+)
