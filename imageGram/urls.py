ba4bbbdc41e6445c38bac3fefd2df0bd207baa17
b3nzuk3 <hussenito7@gmail.com>
2022-05-31 10:01:35 +0300

    search functionality is working. User is able to search for specific content

--- imageGram/urls.py
+++ imageGram/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='image-home'),
+    path('search_results/', views.search, name='image-search'),
 ]
--- imageGram/views.py
+++ imageGram/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from .models import Image
+from django.db.models import Q
 
+
+
+def search(request):
+    ctx={}
+    images = Image.objects.all()
+    if request.method == 'GET':
+        query = request.GET.get("search")
+        query_set = images.filter(
+            Q(name__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(location__name__icontains=query )
+            )
+        total = query_set.count()
+        ctx.update({
+            'total':total,
+            'query':query,
+            'images':query_set
+        })
+
+        return render(request, "imageGram/search.html", ctx)
 
 
 def home(request):
